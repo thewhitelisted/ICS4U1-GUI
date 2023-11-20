9b37a37b032dfe93bc992ab103ebf108a093eaf0
Christopher Lee <clee2k6@outlook.com>
2023-11-20 15:59:11 -0500

    feat: added boilerplate to main class

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+    // Properties
+    JFrame main_frame = new JFrame();
+    JPanel main_panel = new JPanel();
+
+    // Constructor
+    Main() {
+        this.main_panel.setPreferredSize(new Dimension(600, 600));
+        this.main_frame.setContentPane(main_panel);
+        this.main_frame.pack();
+        this.main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.main_frame.setVisible(true);
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        new Main();
+    }
+}
