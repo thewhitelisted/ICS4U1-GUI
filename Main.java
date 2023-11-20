40a63b37e8898a405801e39741995b95aebb5f22
Christopher Lee <clee2k6@outlook.com>
2023-11-20 16:33:37 -0500

    feat: add JMenuBar

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class Main {
@@ -7,8 +13,65 @@ public class Main {
     JFrame main_frame = new JFrame();
     JPanel main_panel = new JPanel();
 
+    JMenuBar main_menubar = new JMenuBar();
+
+    JMenu file_menu = new JMenu("File");
+    JMenuItem save_option = new JMenuItem("Save as CSV");
+    JMenuItem open_option = new JMenuItem("Open CSV file");
+
+    JMenu simulation_menu = new JMenu("Simulation");
+    JMenuItem run_option = new JMenuItem("Run Simulation");
+    JMenuItem reset_option = new JMenuItem("Reset Simulation");
+    JMenuItem clear_option = new JMenuItem("Clear Simulation");
+
     // Constructor
     Main() {
+        // Add JMenuBar to the main frame, and the items to the various menus.
+        this.main_menubar.add(this.file_menu);
+        this.file_menu.add(this.save_option);
+        this.file_menu.add(this.open_option);
+
+        this.main_menubar.add(this.simulation_menu);
+        this.simulation_menu.add(this.run_option);
+        this.simulation_menu.add(this.reset_option);
+        this.simulation_menu.add(this.clear_option);
+
+        this.main_frame.setJMenuBar(this.main_menubar);
+
+        // Add action listeners to the various options
+        this.save_option.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+        this.open_option.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+
+        this.run_option.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+        this.reset_option.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+        this.clear_option.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+            }
+        });
+
+        // Set the defaults for window size, content pane, and default close operation.
         this.main_panel.setPreferredSize(new Dimension(900, 900));
         this.main_frame.setContentPane(main_panel);
         this.main_frame.pack();
