8d6940013c3df5e56790a84548e2fe8704b667a6
Chris Lee <53957527+thewhitelisted@users.noreply.github.com>
2023-11-21 12:51:55 -0500

    fix: adjust triangle position for labels

--- DrawingPanel.java
+++ DrawingPanel.java
@@ -2,13 +2,13 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 
 public class DrawingPanel extends JPanel {
-    int intPointAX = 20;
-    int intPointBX = 20;
-    int intPointCX = 620;
+    int intPointAX = 50;
+    int intPointBX = 50;
+    int intPointCX = 590;
 
-    int intPointAY = 350;
-    int intPointBY = 540;
-    int intPointCY = 540;
+    int intPointAY = 300;
+    int intPointBY = 490;
+    int intPointCY = 490;
 
     public void paintComponent(Graphics g){
         g.fillPolygon(new int[] {intPointAX, intPointBX, intPointCX}, new int[] {intPointAY, intPointBY, intPointCY}, 3);
