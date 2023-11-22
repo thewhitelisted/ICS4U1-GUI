2613d38d3bdeab646ed4bf0f1de298dbb4253492
Christopher Lee <clee2k6@outlook.com>
2023-11-21 20:16:10 -0500

    feat: added button and changed ActionListeners

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -10,7 +19,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Main {
+public class Main implements ActionListener {
     // Properties
     JFrame main_frame = new JFrame();
     JPanel container_panel = new JPanel();
@@ -39,6 +48,48 @@ public class Main {
     JTextField kinetic_friction_field = new JTextField();
     JLabel force_applied_label = new JLabel("Force Applied:");
     JTextField force_applied_field = new JTextField();
+    JButton load_settings_button = new JButton("Load Settings in Simulation");
+
+    // Methods
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        if (e.getSource() == this.load_settings_button) {
+            System.out.println("Load Settings in Simulation");
+        } else if (e.getSource() == this.save_option) {
+            this.saveSettings();
+        } else if (e.getSource() == this.open_option) {
+            this.loadSettings();
+        } else if (e.getSource() == this.run_option) {
+            System.out.println("Run Simulation");
+        } else if (e.getSource() == this.reset_option) {
+            System.out.println("Reset Simulation");
+        } else if (e.getSource() == this.clear_option) {
+            System.out.println("Clear Simulation");
+        }
+    }
+
+    private void saveSettings() {
+        // save settings to a CSV file
+        // format: angle, mass, static friction, kinetic friction, force applied
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("settings.csv"));
+            pw.println(this.angle_field.getText() + "," + this.mass_field.getText() + ","
+                    + this.static_friction_field.getText() + "," + this.kinetic_friction_field.getText() + ","
+                    + this.force_applied_field.getText());
+            pw.close();
+        } catch (IOException e) {
+        }
+    }
+
+    private void loadSettings() {
+        // load settings from a CSV file
+        // load angle, mass, static friction, kinetic friction, force applied in that
+        // order
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("settings.csv"));
+        } catch (FileNotFoundException e) {
+        }
+    }
 
     // Constructor
     Main() {
@@ -56,12 +107,14 @@ public class Main {
 
         // Add action listeners to the various options, see GUIListener for more
         // information
-        this.save_option.addActionListener(new GUIListener(this.save_option));
-        this.open_option.addActionListener(new GUIListener(this.open_option));
+        this.save_option.addActionListener(this);
+        this.open_option.addActionListener(this);
 
-        this.run_option.addActionListener(new GUIListener(this.run_option));
-        this.reset_option.addActionListener(new GUIListener(this.reset_option));
-        this.clear_option.addActionListener(new GUIListener(this.clear_option));
+        this.run_option.addActionListener(this);
+        this.reset_option.addActionListener(this);
+        this.clear_option.addActionListener(this);
+
+        this.load_settings_button.addActionListener(this);
 
         // Set the defaults for window size, content pane, and default close operation.
         this.container_panel.setLayout(new BoxLayout(this.container_panel, BoxLayout.X_AXIS));
@@ -76,28 +129,30 @@ public class Main {
         this.drawing_panel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         // Add labels and textfields to the main panel
-        this.title_label.setBounds(10, 5, 200, 25);
+        this.title_label.setBounds(10, 5, 210, 25);
         this.main_panel.add(this.title_label);
         this.angle_label.setBounds(10, 40, 50, 25);
         this.main_panel.add(this.angle_label, JLabel.LEFT_ALIGNMENT);
-        this.angle_field.setBounds(60, 40, 230, 25);
+        this.angle_field.setBounds(60, 40, 240, 25);
         this.main_panel.add(this.angle_field);
         this.mass_label.setBounds(10, 75, 115, 25);
         this.main_panel.add(this.mass_label);
-        this.mass_field.setBounds(115, 75, 175, 25);
+        this.mass_field.setBounds(115, 75, 185, 25);
         this.main_panel.add(this.mass_field);
         this.static_friction_label.setBounds(10, 110, 200, 25);
         this.main_panel.add(this.static_friction_label);
-        this.static_friction_field.setBounds(200, 110, 90, 25);
+        this.static_friction_field.setBounds(200, 110, 100, 25);
         this.main_panel.add(this.static_friction_field);
         this.kinetic_friction_label.setBounds(10, 145, 200, 25);
         this.main_panel.add(this.kinetic_friction_label);
-        this.kinetic_friction_field.setBounds(200, 145, 90, 25);
+        this.kinetic_friction_field.setBounds(200, 145, 100, 25);
         this.main_panel.add(this.kinetic_friction_field);
         this.force_applied_label.setBounds(10, 180, 100, 25);
         this.main_panel.add(this.force_applied_label);
-        this.force_applied_field.setBounds(100, 180, 190, 25);
+        this.force_applied_field.setBounds(100, 180, 200, 25);
         this.main_panel.add(this.force_applied_field);
+        this.load_settings_button.setBounds(10, 215, 290, 25);
+        this.main_panel.add(this.load_settings_button);
 
         this.main_frame.setContentPane(container_panel);
         this.main_frame.pack();
