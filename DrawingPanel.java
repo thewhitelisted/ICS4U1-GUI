1dfc08de8e3c188ffab89b9a4dc75be912125711
thewhitelisted <clee2k6@outlook.com>
2023-12-05 10:50:04 -0500

    fix: increase acceleration, base is 135 meter

--- DrawingPanel.java
+++ DrawingPanel.java
@@ -125,7 +125,7 @@ public class DrawingPanel extends JPanel {
         }
         dblAccelerationX = ((Math.sin(Math.toRadians(dblDegrees))) - (dblKineticFriction * Math.cos(Math.toRadians(dblDegrees)))) * dblGravity * Math.cos(Math.toRadians(dblDegrees));
         System.out.println(dblAccelerationX);
-        dblVelX = (dblAccelerationX/48) * dblTime;
+        dblVelX = (dblAccelerationX/12) * dblTime;
         if (dblVelX < 0){
             dblVelX = 0;
         }
